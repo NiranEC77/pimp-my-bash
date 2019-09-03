8172319349d2f08322ab5b3ecc04a8d8c40d5c3e
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2019-09-03 15:54:47 -0400

    Create kube-ps1-install.sh

---
+++ kube-ps1-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://github.com/jonmosco/kube-ps1/archive/v0.7.0.tar.gz -P /tmp
+tar -zxf /tmp/v0.7.0.tar.gz -C /tmp
+chmod +x /tmp/kube-ps1-0.7.0/kube-ps1.sh
+source /tmp/kube-ps1-0.7.0/kube-ps1.sh
+/tmp/kube-ps1-0.7.0/kube-ps1.sh
+kubeon -g
+chmod +x kube-ps1-startup.sh
+echo "bash --init-file /root/install-kubeps1/kube-ps1-startup.sh" >>/root/.profile
+exit 0
