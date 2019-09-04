d209bf8cfa293723b8d554f67837dcd4ea5eded3
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2019-09-03 21:56:54 -0400

    Update pimp-my-bash.sh

--- pimp-my-bash.sh
+++ pimp-my-bash.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 apt-get -y install jq
+
+#Install pv for the demo-scripts
+apt-get -y install pv
+
+#Install kube-ps1
 source kube-ps1/kube-ps1-install.sh
+
+#Install kubectx
 source kubectx/install-kubctx.sh
 exit 0
