dd928dc8a7fbc7abdcd843b4c5579fe47b970ada
niranec77 <niranec@gmail.com>
2020-01-13 11:24:17 -0800

    added aliases

---
+++ aliases/aliases.sh
@@ -0,0 +1,13 @@
+alias kc='kubectl'
+
+alias kgp='kubectl get pods'
+
+alias kgs='kubectl get svc'
+
+alias kgc='kubectl get componentstatuses’
+
+
+
+alias kgc='kubectl config get-context'
+
+alias h='history |grep'
