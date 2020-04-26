e063eb1c838e8f409fc01aea5bee4dfb87bedabd
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2020-04-26 17:15:36 -0400

    Update pimp-my-bash.sh

--- pimp-my-bash.sh
+++ pimp-my-bash.sh
@@ -5,9 +5,15 @@ apt-get -y install jq
 apt-get -y install pv
 
 #Install kube-ps1
+git clone https://github.com/jonmosco/kube-ps1
 chmod +x kube-ps1/kube-ps1-install.sh
-source kube-ps1/kube-ps1-install.sh
+source ~/kube-ps1/kube-ps1.sh
+PS1='[\e[0;33m\w $(kube_ps1)]\$ '
+
 
 #Install kubectx
-source kubectx/install-kubctx.sh
+alias kubectx='kubectl config get-contexts'
+alias kubeset='kubectl config set-context'
+alias k=kubectl
+alias h='history |grep'
 exit 0
