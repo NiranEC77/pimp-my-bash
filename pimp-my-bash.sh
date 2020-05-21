476965c0cdb9b6f283986de9e646c3a6c33f11de
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2020-05-21 10:00:42 -0400

    Update pimp-my-bash.sh

--- pimp-my-bash.sh
+++ pimp-my-bash.sh
@@ -6,7 +6,7 @@ apt-get -y install pv
 
 #Install kube-ps1
 git clone https://github.com/jonmosco/kube-ps1
-chmod +x kube-ps1/kube-ps1-install.sh
+chmod +x kube-ps1/kube-ps1.sh
 source ~/kube-ps1/kube-ps1.sh
 PS1='[\e[0;33m\w $(kube_ps1)]\$ '
 
