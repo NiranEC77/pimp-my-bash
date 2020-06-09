79d815e2c79cd40a4c6329fa5adfc2e1a0b3c8ef
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2020-06-09 08:55:19 -0400

    Update pimp-my-bash.sh

--- pimp-my-bash.sh
+++ pimp-my-bash.sh
@@ -7,13 +7,49 @@ apt-get -y install pv
 #Install kube-ps1
 git clone https://github.com/jonmosco/kube-ps1
 chmod +x kube-ps1/kube-ps1.sh
-source ~/kube-ps1/kube-ps1.sh
-PS1='[\e[0;33m\w $(kube_ps1)]\$ '
+
+#source ~/kube-ps1/kube-ps1.sh
+#PS1='[\e[0;33m\w $(kube_ps1)]\$ '
+
+#Install zsh
+sudo apt install zsh
+
+#Install ohmyzsh
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Change Theme
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+ 
+#Change default shell
+sudo chsh -s $(which zsh)
 
 
-#Install kubectx
+#Alias
 alias kubectx='kubectl config get-contexts'
 alias kubeset='kubectl config use-context'
 alias k=kubectl
 alias h='history |grep'
-exit 0
+
+
+PROMPT='%(?.%F{green}√.%F{red}?%?)%f %B%F{yellow}%1~%f%b %#'
+source /root/kube-ps1/kube-ps1.sh
+PROMPT='$(kube_ps1)'$PROMPT
+
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+RPROMPT=\$vcs_info_msg_0_
+zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
+zstyle ':vcs_info:*' enable git
+
+
+#If zsh not working run 'zsh' to test and add the follwing to .bashrc #
+#https://blog.joaograssi.com/windows-subsystem-for-linux-with-oh-my-zsh-conemu/
+#if test -t 1; then
+#exec zsh
+#fi
+
+#To uninstall zsh https://askubuntu.com/questions/963874/uninstall-oh-my-zsh
+#sudo chmod 777 ~/.oh-my-zsh/tools/uninstall.sh
+#~/.oh-my-zsh/tools/uninstall.sh
