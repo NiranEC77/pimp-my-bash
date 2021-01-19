604d26b9554c39741f7635355d2017c9d432d752
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2021-01-19 09:57:21 -0500

    Update pimp-my-bash.sh

--- pimp-my-bash.sh
+++ pimp-my-bash.sh
@@ -1,12 +1,12 @@
 !/bin/bash
-apt-get -y install jq
+sudo apt-get -y install jq
 
 #Install pv for the demo-scripts
-apt-get -y install pv
+sudo apt-get -y install pv
 
 #Install kube-ps1
-git clone https://github.com/jonmosco/kube-ps1
-chmod +x kube-ps1/kube-ps1.sh
+sudo git clone https://github.com/jonmosco/kube-ps1
+sudo chmod +x kube-ps1/kube-ps1.sh
 
 #source ~/kube-ps1/kube-ps1.sh
 #PS1='[\e[0;33m\w $(kube_ps1)]\$ '
